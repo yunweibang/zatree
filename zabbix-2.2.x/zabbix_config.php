8dbc61c47aed085059a4d1658f4e179805a5ed2f
qichengDtur <568543779@qq.com>
2014-07-18 11:06:15 +0800

    Update zabbix_config.php
    添加HTTP Basic Athorization认证用户名和密码参数

--- zabbix-2.2.x/zabbix_config.php
+++ zabbix-2.2.x/zabbix_config.php
@@ -3,6 +3,8 @@ global $zabbix_api_config;
 
 $zabbix_api_config=array(
  'api_url'=>'api_jsonrpc.php',
+ 'http_user'=>'xxxxx',
+ 'http_password'=>'xxxxx',
  'user'=>'admin',
  'passowrd'=>'xxxxx',
  'graph_url'=>'zabbix_chart.php',
